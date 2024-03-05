74dc40666ec0baa874f0fc319e8b7fad32ad3dc5
Brayden H <35241666+hord-brayden@users.noreply.github.com>
2024-03-05 08:53:55 -0700

    Create radiusFind.js

---
+++ radiusFind.js
@@ -0,0 +1,6 @@
+function findUnitCircle(circleRadius){
+    // finds the area of a circle with provided radius
+    // typically equates to the radius squared
+    var sqPi = Math.sqrt(1/Math.PI);
+    var areaCheck = Math.PI * Math.pow((sqPi * circleRadius), 2)
+}
